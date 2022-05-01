6aadededd14733a045d446d1e7f959b488b5e8d4
TheLastMelancholy <uadrugusserko@yandex.ru>
2022-05-01 12:39:16 +0300

    Feat: attempt to apply homography matrix to image dimentions

--- Processor.py
+++ Processor.py
@@ -76,15 +76,22 @@ class meta_image():
         self.image = None
         self.h = None
         self.w = None
-        self.H = None
+        self.homographyMatrix = None
+        self.homographyMatrixInverted = None
 
     def __repr__(self):
         return f"{self.image_path}: {self.perspective}"
 
-    def get_perspective_mat(self, target_meta):
+    def calculate_perspective_mat(self, target_meta):
         H, _ = cv.findHomography(self.perspective.as_array(),
                                  target_meta.as_array())
-        self.H = H
+        print(f"Homography Matrix are\n{H}")
+        HInv, _ = cv.findHomography(target_meta.as_array(),
+                                    self.perspective.as_array())
+        print(f"Homography Matrix Inverted are \n{HInv}")
+
+        self.homographyMatrix = H
+        self.homographyMatrixInverted = HInv
 
     def read_image_dimentions(self):
         img = cv.imread(self.image_path)
@@ -96,15 +103,30 @@ class meta_image():
         img = cv.imread(self.image_path)
         return img
 
-    def apply_perspective(self, target_meta):
+    def apply_perspective(self, target_meta, target_shape):
         img = self.read_image()
-        # corrected = cv.warpPerspective(img, perspective_mat, (img1.shape[1], img1.shape[0]))
+        corrected = cv.warpPerspective(
+            img, self.homographyMatrix, (target_shape[1], target_shape[0]))
+        return corrected
 
     def calculate_corrected_size(self):
-        if self.perspective is None or self.h is None or self.w is None:
+        if self.homographyMatrix is None or \
+                self.homographyMatrixInverted is None or \
+                self.h is None or \
+                self.w is None:
             raise Exception("Image paramters are not defined")
-        ...
+        h_corner = np.asarray([self.h, 0.0, 1.0])
+        w_corner = np.asarray([0.0, self.w, 1.0])
 
+        rev_h_meta = np.dot(self.homographyMatrix, h_corner)
+        rev_h_meta_inv = np.dot(self.homographyMatrixInverted, h_corner)
+        rev_w_meta = np.dot(self.homographyMatrix, w_corner)
+        rev_w_meta_inv = np.dot(self.homographyMatrixInverted, w_corner)
+
+        print(f"rev_h_meta = {rev_h_meta}")
+        print(f"rev_h_meta_inv = {rev_h_meta_inv}")
+        print(f"rev_w_meta = {rev_w_meta}")
+        print(f"rev_w_meta_inv = {rev_w_meta_inv}")
 
 
 def average_perspective(meta_perspectives):
@@ -114,16 +136,23 @@ def average_perspective(meta_perspectives):
     return total_meta.divide(len(meta_perspectives))
 
 
-test_files_directory = os.path.join(os.getcwd(),"Test/correction_log.log")
+test_files_directory = os.path.join(os.getcwd(), "Test/correction_log.log")
 
 meta_images = []
 
 reader = file_reader(test_files_directory)
+
+LAST_ADDED = -1
+
 for line in reader:
     meta_images.append(meta_image(line, default_parser))
-    print(meta_images[-1])
-    meta_images[-1].read_image_dimentions()
-    print(meta_images[-1].h)
-    print(meta_images[-1].w)
+    print(meta_images[LAST_ADDED])
+    meta_images[LAST_ADDED].read_image_dimentions()
+    print(meta_images[LAST_ADDED].h)
+    print(meta_images[LAST_ADDED].w)
 
 central_meta = average_perspective([_.perspective for _ in meta_images])
+
+for meta_image in meta_images:
+    meta_image.calculate_perspective_mat(central_meta)
+    meta_image.calculate_corrected_size()
